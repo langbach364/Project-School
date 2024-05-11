14179d29a19401cb755dbc71099eb18ba1bf05e3
bachlang <bachlang364@gmail.com>
2024-05-11 18:05:42 +0700

    branching frontend

--- backend/Security/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("algorithm after security:", encode_data1("1", "DELETE", 2))
-}
--- backend/System/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.22.2
-
-require github.com/rs/cors v1.11.0
